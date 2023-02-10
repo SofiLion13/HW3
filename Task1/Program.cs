d13d6cce9c20feaf5b21882b971ca6cfe55dc00d
Sofia <dragsy13@bk.ru>
2023-02-10 11:11:23 +0300

    Add new task

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,3 +4,45 @@
 // 12821 -> да
 // 23432 -> да
 
+// using System;
+// public class IsPalindrom
+// {
+//     public static void Main (string[] args)
+//     {
+//         int number, total = 0, t, reverse;
+//         Console.Write("Введите пятизначное число:");
+//         number = int.Parse(Console.ReadLine());
+//         t = number;
+//         while (number > 0)
+//         {
+//             reverse = number % 10;
+//             total = (total * 10) + reverse;
+//             number = number / 10;
+//         }
+//         if (t == total)
+//         Console.Write("Палиндром");
+//         else
+//         Console.Write("He палиндром");
+//     }
+// }
+
+class IsPalindrom
+{
+    static void Main (string[] args)
+    {
+        int number, total = 0, t, reverse;
+        Console.Write("Введите пятизначное число: ");
+        number = int.Parse(Console.ReadLine());
+        t = number;
+        while (number > 0)
+        {
+            reverse = number % 10;
+            total = (total * 10) + reverse;
+            number = number / 10;
+        }
+        if (t == total)
+        Console.Write("Палиндром");
+        else
+        Console.Write("He палиндром");
+    }
+}
